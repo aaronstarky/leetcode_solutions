f5ef2009e10206adfca7e0592968b64af5d1da24
Aaron Starkweather surface 2099 <aarondstarkweather@gmail.com>
2025-07-30 13:18:17 -0500

    Add note about alternate solution

--- lc242_ValidAnagram.py
+++ lc242_ValidAnagram.py
@@ -11,3 +11,24 @@ class Solution:
             if s_letters[key] != t_letters.get(key, 0):
                 return False
         return True
+    
+    
+class AlternateSolution:
+    '''
+    This solution is super smart
+    Instead of storing two dictionaries, he stores one
+    Then he subtracts from the value that was stored of occurences of a char in s
+    If he reaches zero and encounters another instance, it fails
+    But if the string is an anagram, he will never encounter another instance so the zero will never be seen again.
+    '''
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        dic = {}
+        for c in s:
+            dic[c] = dic.get(c, 0) + 1
+        for c in t:
+            if dic.get(c, 0) == 0:
+                return False
+            dic[c] -= 1
+        return True
