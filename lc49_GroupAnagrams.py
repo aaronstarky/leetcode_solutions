2254909d5b77f430331e738b836fbc3f7acffb48
Aaron Starkweather surface 2099 <aarondstarkweather@gmail.com>
2025-08-07 21:14:43 -0500

    added extra solutions to lc49

--- lc49_GroupAnagrams.py
+++ lc49_GroupAnagrams.py
@@ -1,3 +1,9 @@
+'''
+First solution
+
+runtime: O(n^2m) with n as the number of words and m being the length of the longest word
+space: O(nm) with n being the number of words and m being the length of the longest word
+'''
 class Solution(object):
     def groupAnagrams(self, strs):
         """
@@ -39,4 +45,53 @@ class Solution(object):
             d[c] += 1
         self.dicts[i] = d
         return d
-        
+
+'''
+Second solution that uses sorting like I wanted to originally
+
+run: O(n^2 log n)
+space: O(nm)
+'''    
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        words = {}
+        for i in range(len(strs)):
+            srtd = "".join(sorted(strs[i]))
+            if srtd not in words:
+                words[srtd] = []
+            words[srtd].append(strs[i])
+        result = []
+        for key in words:
+            result.append(words[key])
+        return result
+    
+    
+'''
+this is a random solution that was minimizing space
+'''
+class Solution(object):
+    def anagram(self,a,b):
+        m1 = {}
+        m2 = {}
+        for i in range(len(a)):
+            m1[a[i]]=m1.get(a[i],0)+1
+            m2[b[i]]=m2.get(b[i],0)+1
+        return m1==m2
+    def groupAnagrams(self, strs):
+        ana=[[strs[0]]]
+        for i in range(1,len(strs)):
+            for w in range(len(ana)):
+                if (len(strs[i])==len(ana[w][0])) and (set(strs[i])==set(ana[w][0])):
+                    if self.anagram(strs[i],ana[w][0]):
+                        #print(strs[i],ana[w])
+                        temp=ana[w]
+                        temp.append(strs[i])
+                        ana[w]=temp
+                        break
+            else:
+                ana.append([strs[i]])
+        return ana
