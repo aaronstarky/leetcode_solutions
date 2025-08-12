a1fa0eb3178119869cd4a0cee665e675f230f08a
Aaron Starkweather surface 2099 <aarondstarkweather@gmail.com>
2025-08-12 13:19:53 -0600

    add lc128

---
+++ lc128_LongestConsecutiveSubsequence.py
@@ -0,0 +1,18 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+        # sort the array
+        nums.sort()
+        # create needed values
+        dp = [1 for i in range(len(nums))]
+        curr = 1
+        for i in range(1, len(nums)):
+            if nums[i]-1 == nums[i-1]:
+                curr += 1
+            elif nums[i]-1 > nums[i-1]:
+                curr = 1
+            dp[i] = max(curr, dp[i-1])
+        return dp[-1]
+
+        
